4a0596715f2337114e03b82b0e3c0bc63932d8da
Madcodes831 <r.medina2119@gmail.com>
2024-05-27 18:41:00 -0700

    nav title edits

--- src/app/nav/Nav.tsx
+++ src/app/nav/Nav.tsx
@@ -1,15 +1,17 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import { HamburgerMenu } from './ui/HamburgerMenu';
+import React from "react";
+import Link from "next/link";
+import { HamburgerMenu } from "./ui/HamburgerMenu";
 
 const Nav: React.FC = () => {
   return (
     <header className="nav-container">
-      <div className="nav">
+      <div className="title-container">
         <h1 className="nav-title">Timeless Memories</h1>
-        
+      </div>
+
+      <div className="nav">
         <div className="regular-nav">
           <Link href="/" className="nav-link">
             Home
@@ -22,16 +24,16 @@ const Nav: React.FC = () => {
           <Link href="/videos" className="nav-link">
             Videos
           </Link>
-          
+
           <Link href="/signin" className="nav-link">
             Sign In
           </Link>
         </div>
-        
+
         <HamburgerMenu />
       </div>
     </header>
   );
-}
+};
 
 export default Nav;
